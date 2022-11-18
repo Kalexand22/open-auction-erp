6fe11f3e6c65c5c12048fdb3af6467854f441b0e
Kalexand22 <karl.alexandersson@it-innovations-bzh.fr>
2022-11-18 10:39:14 +0000

    ok

---
+++ modules/open-auction-base/src/main/java/com/axelor/apps/openauctionbase/csv/ImportPicture.java
@@ -0,0 +1,41 @@
+package com.axelor.apps.openauctionbase.csv;
+
+import com.axelor.apps.openauction.db.Lot;
+import com.axelor.meta.MetaFiles;
+import com.axelor.meta.db.MetaFile;
+import com.google.common.base.Strings;
+import com.google.inject.Inject;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ImportPicture {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ImportPicture.class);
+
+  @Inject private MetaFiles metaFiles;
+
+  public Object importMainPicture(Object bean, Map<String, Object> values) {
+
+    Lot lot = (Lot) bean;
+
+    final Path path = (Path) values.get("__path__");
+    String fileName = (String) values.get("mainPicture_fileName");
+
+    if (!Strings.isNullOrEmpty(fileName)) {
+      try {
+        final File image = path.resolve("img" + File.separator + fileName).toFile();
+        if (image.exists()) {
+          final MetaFile metaFile = metaFiles.upload(image);
+          lot.setMainPicture(metaFile);
+        }
+      } catch (Exception e) {
+        LOG.warn("Can't load image {} for app {}", fileName, lot.getName());
+      }
+    }
+
+    return lot;
+  }
+}
